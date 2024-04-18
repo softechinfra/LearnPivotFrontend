356068a7f058a28b77fba4146fa352f9c1293a47
viveksinra <vivekkrvks@gmail.com>
2024-04-18 16:08:36 +0530

    build softechinfra is working here now

--- app/MyApp.jsx
+++ app/MyApp.jsx
@@ -14,6 +14,7 @@ import {authService} from "./services/index"
 import Loading from "./Components/Loading/Loading";
 import Hpage1 from './Components/PublicPage/HomePage/Hpage1';
 import HeroSec from './Components/PublicPage/HomePage/HeroSec';
+import Banner from "./Components/ITStartup/Banner";
 
 
 function MyApp() {  
@@ -42,7 +43,8 @@ function MyApp() {
       <Header />
       <Suspense fallback={<Loading />}>
         <TopAbstract />
-   <HeroSec />
+        <Banner />
+   {/* <HeroSec /> */}
         <Hpage1 />
         <Box >
           <Suspense fallback={<Loading />}>
